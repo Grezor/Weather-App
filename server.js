9f87202e8679f535bfeb10fb4330b9dec93ba62d
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-05-22 12:00:47 +0200

    start api weather

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
+const request = require('request')
+const apiKey = '';
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!')
-// })
+
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
+app.get('/', function (req, res) {
+  res.render('index');
+})
 
 app.post('/', function (req, res) {
-  // NEW CODE
   res.render('index');
+  console.log(req.body.city)
 })
 
 app.listen(3000, function () {
