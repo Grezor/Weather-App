a8d3af3cdba016f41aeba2e18ffabae4610aaf78
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-05-22 11:42:17 +0200

    create file js and css

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+
+
+app.post('/', function (req, res) {
+  // NEW CODE
+  res.render('index');
 })
 
 app.listen(3000, function () {
