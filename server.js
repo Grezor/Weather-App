f768451d7e886c0d1d7a1331145684e2dff8c79b
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-05-23 23:24:35 +0200

    add picture, and weather infos

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express()
 require('dotenv').config({
   path: __dirname + '/.env'
 })
+// clé
 const apiKey = `${process.env.APIKEY}`
 
 
@@ -23,8 +24,15 @@ app.get('/', function (req, res) {
 })
 // les icons
 const weatherIcons = {
-  '04d': 'sun.png',
-  '04n': 'moon.png',
+  '01d': '/public/img/clearSky.png',
+  '02d': '/public/img/fewClouds.png',
+  '03d': '/public/img/scatteredClouds.png',
+  '04d': '/public/img/brokenClouds.png',
+  '09d': '/public/img/showerRain.png',
+  '10d': '/public/img/rain.png',
+  '11d': '/public/img/thunderstorm.png',
+  '13d': '/public/img/snow.png',
+  '50d': '/public/img/mist.png',
   'err': 'err.png'
 }
 // pour afficher les icons
@@ -32,19 +40,25 @@ function getIcon(weatherIcon) {
   const weatherKey = weatherIcon in weatherIcons ? weatherIcon : 'err'
   return weatherIcons[weatherKey]
 }
-
+/**
+ * @param {*} weatherResponse 
+ */
 function transformWeatherResponse(weatherResponse) {
   if (weatherResponse.main == undefined) {
     return {
       weather: null,
-      error: 'Error, please try again'
+      error: 'Erreur'
     }
   }
-  
+
   const weather = {
+    name: weatherResponse.name,
     temp: Math.round((weatherResponse.main.temp * 100) / 100),
     icon: getIcon(weatherResponse.weather[0].icon),
-    humidity: weatherResponse.main.humidity
+    humidity: weatherResponse.main.humidity,
+    temp_max: weatherResponse.main.temp_max,
+    temp_min: weatherResponse.main.temp_min,
+    speed: weatherResponse.wind.speed,
   }
 
   return {
@@ -55,7 +69,7 @@ function transformWeatherResponse(weatherResponse) {
 
 app.post('/', function (req, res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=fr`
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=fr&wind=Imperial`
 
   request(url, function (err, response, body) {
     if (err) {
@@ -66,10 +80,12 @@ app.post('/', function (req, res) {
     }
 
     const weatherResponse = JSON.parse(body)
-    res.render('index', transformWeatherResponse(weatherResponse))
+    res.render('index',transformWeatherResponse(weatherResponse) );
   });
 })
-
+/**
+* Serveur web : 
+*/
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('le serveur est sur le port 3000!')
 })
