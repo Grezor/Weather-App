5553aa90fb892a5f7a72c274f2ca57eb6defd145
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-05-23 13:53:41 +0200

    refactorisation code

---
+++ README.md
@@ -0,0 +1,21 @@
+# Weather App
+
+## ▶️ Présentation :
+Ce projet, est une application web me permettant d'afficher la météo, la température, ou bien le taux d'humidité.
+
+## ▶️ Technologie : 
+    - Javascript
+    - NodeJS
+    - CSS 
+
+## ▶️ Requis :
+Pour faire fonctionner cette todolist, il vous faudra d'installer :
+    - NodeJS [Installation](https://nodejs.org/en/)
+    - Postman [Installation](https://www.postman.com/downloads/)
+
+## ▶️ Ressources :
+
+## ▶️ Tests :
+
+## ▶️ Auteur
+- **Duplessi Geoffrey** 
--- server.js
+++ server.js
@@ -1,21 +1,73 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const app = express()
-const request = require('request')
-const apiKey = '';
+// api meteo
+require('dotenv').config({
+  path: __dirname + '/.env'
+})
+const apiKey = `${process.env.APIKEY}`
 
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {
+    weather: null,
+    error: null
+  });
 })
+// les icons
+const weatherIcons = {
+  '04d': 'sun.png',
+  '04n': 'moon.png',
+  'err': 'err.png'
+}
+// pour afficher les icons
+function getIcon(weatherIcon) {
+  const weatherKey = weatherIcon in weatherIcons ? weatherIcon : 'err'
+  return weatherIcons[weatherKey]
+}
+
+function transformWeatherResponse(weatherResponse) {
+  if (weatherResponse.main == undefined) {
+    return {
+      weather: null,
+      error: 'Error, please try again'
+    }
+  }
+  
+  const weather = {
+    temp: Math.round((weatherResponse.main.temp * 100) / 100),
+    icon: getIcon(weatherResponse.weather[0].icon),
+    humidity: weatherResponse.main.humidity
+  }
+
+  return {
+    weather,
+    error: null
+  }
+}
 
 app.post('/', function (req, res) {
-  res.render('index');
-  console.log(req.body.city)
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=fr`
+
+  request(url, function (err, response, body) {
+    if (err) {
+      return res.render('index', {
+        weather: null,
+        error: 'Error, please try again'
+      });
+    }
+
+    const weatherResponse = JSON.parse(body)
+    res.render('index', transformWeatherResponse(weatherResponse))
+  });
 })
 
 app.listen(3000, function () {
