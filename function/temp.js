ba5e333a7493877a620d2617df347652ae7c4851
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-06-07 13:48:31 +0200

    install eslint, and clean code

--- function/temp.js
+++ function/temp.js
@@ -1,27 +1,25 @@
 module.exports = {
-  /**
-  * Couchée du soleil
-  * return format heure (21:30)
-  */
-  sunset: (sunsetTime) => {
-    const date = new Date(sunsetTime * 1000);
-    const hours = date.getHours();
-    const minutes = "0" + date.getMinutes();
-    const formattedTime = hours + ':' + minutes.substr(-2);
-    return formattedTime
-  },
-  /**
+    /**
+    * Couchée du soleil
+    * return format heure (21:30)
+    */
+    sunset: (sunsetTime) => {
+        const date = new Date(sunsetTime * 1000)
+        const hours = date.getHours()
+        const minutes = '0' + date.getMinutes()
+        const formattedTime = hours + ':' + minutes.substr(-2)
+        return formattedTime
+    },
+    /**
   * levée du soleil
   * return format heure (05:30)
   */
-  sunrise: (sunrise) => {
-    const unix_timestamp = sunrise
-    const date = new Date(unix_timestamp * 1000);
-    const hours = date.getHours();
-    const minutes = "0" + date.getMinutes();
-    const heureleve = hours + ':' + minutes.substr(-2);
-    return heureleve
-  }
-
+    sunrise: (sunrise) => {
+        const unix_timestamp = sunrise
+        const date = new Date(unix_timestamp * 1000)
+        const hours = date.getHours()
+        const minutes = '0' + date.getMinutes()
+        const heureleve = hours + ':' + minutes.substr(-2)
+        return heureleve
+    }
 }
-
--- server.js
+++ server.js
@@ -1,93 +1,88 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const app = express();
+/* eslint-disable no-undef */
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
 
 const temp = require('./function/temp')
-const  icon = require('./function/icon')
-const  city = require('./function/city')
+const icon = require('./function/icon')
+
 // api meteo
 require('dotenv').config({
-  path: __dirname + '/.env'
+    path: __dirname + '/.env'
 })
 // clé
 const apiKey = `${process.env.APIKEY}`
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
-  extended: true
-}));
+    extended: true
+}))
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index', {
-    weather: null,
-    error: null
-  });
+    res.render('index', {
+        weather: null,
+        error: null
+    })
 })
-
-
 
 app.get('/api2', function (req, res) {
-  res.render('api2', {
-    weather: null,
-    error: null
-  });
+    res.render('api2', {
+        weather: null,
+        error: null
+    })
 })
 
-/**
- * @param {*} weatherResponse 
- */
 function transformWeatherResponse(weatherResponse) {
-  if (!weatherResponse.main) {
-    return {
-      weather: null,
-      error: 'Erreur'
+    if (!weatherResponse.main) {
+        return {
+            weather: null,
+            error: 'Erreur'
+        }
     }
-  }
 
-  const weather = {
-    name: weatherResponse.name,
-    coord : weatherResponse.coord.lon,
-    main: weatherResponse.main.humidity,
-    temp: Math.round((weatherResponse.main.temp * 100) / 100),
-    icon: icon.getIcon(weatherResponse.weather[0].icon),
-    humidity: weatherResponse.main.humidity,
-    temp_max: weatherResponse.main.temp_max,
-    temp_min: weatherResponse.main.temp_min,
-    speed: Math.round(weatherResponse.wind.speed * 3.6),
-    sunset: temp.sunset(weatherResponse.sys.sunset),
-    sunrise: temp.sunrise(weatherResponse.sys.sunrise),
-    lastUpdate: weatherResponse.lastupdate
-  }
-
-  return {
-    weather,
-    error: null
-  }
+    const weather = {
+        name: weatherResponse.name,
+        coord: weatherResponse.coord.lon,
+        main: weatherResponse.main.humidity,
+        temp: Math.round((weatherResponse.main.temp * 100) / 100),
+        icon: icon.getIcon(weatherResponse.weather[0].icon),
+        humidity: weatherResponse.main.humidity,
+        temp_max: weatherResponse.main.temp_max,
+        temp_min: weatherResponse.main.temp_min,
+        speed: Math.round(weatherResponse.wind.speed * 3.6),
+        sunset: temp.sunset(weatherResponse.sys.sunset),
+        sunrise: temp.sunrise(weatherResponse.sys.sunrise),
+        lastUpdate: weatherResponse.lastupdate
+    }
+    return {
+        weather,
+        error: null
+    }
 }
 
 app.post('/', function (req, res) {
 
-  const city = req.body.city;
-  const lang = 'fr'
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${lang}&units=metric&wind=Metric&appid=${apiKey}&lang=fr`
+    const city = req.body.city
+    const lang = 'fr'
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${lang}&units=metric&wind=Metric&appid=${apiKey}&lang=fr`
 
-  request(url, function (err, response, body) {
-    if (err) {
-      return res.render('index', {
-        weather: null,
-        error: 'Error, please try again'
-      });
-    }
+    request(url, function (err, response, body) {
+        if (err) {
+            return res.render('index', {
+                weather: null,
+                error: 'Error, please try again'
+            })
+        }
 
-    const weatherResponse = JSON.parse(body)
-    res.render('index', transformWeatherResponse(weatherResponse));    
-  });
+        const weatherResponse = JSON.parse(body)
+        res.render('index', transformWeatherResponse(weatherResponse))
+    })
 })
 /**
-* Serveur web : 
-*/
+ * Serveur web : 
+ */
 app.listen(3000, function () {
-  console.log('le serveur est sur le port 3000!')
+    console.log('le serveur est sur le port 3000!')
 })
