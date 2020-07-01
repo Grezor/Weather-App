1a17035eea941572ae1734ae216f7f32cd5063a3
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-07-01 22:10:56 +0200

    add card css

--- function/api2.js
+++ function/api2.js
@@ -22,9 +22,8 @@ const api2 = {
             weather_main: daily.weather[0].main,
             weather_icon: daily.weather[0].icon,
             // 
-            sunrise: daily.sunrise,
-            sunset: daily.sunset
-
+            sunrise: api2.timestampToHour(daily.sunrise),
+            sunset: api2.timestampToHour(daily.sunset)
         }
     },
     /**
@@ -36,8 +35,18 @@ const api2 = {
         const year = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
         const month = new Intl.DateTimeFormat('fr', { month: '2-digit' }).format(date)
         const day = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
-        
         return `${day}/${month}/${year}`
+    },
+    /**
+    * Convertie le timestamp du couché et levé du soleil
+    * en heure
+    */
+    timestampToHour: (clock) => {
+        const date = new Date(clock * 1000)
+        const hours = date.getHours()
+        const minutes = '0' + date.getMinutes()
+        const formattedTime = hours + ':' + minutes.substr(-2)
+        return formattedTime
     },
 
     respons2: (weatherResponse) => {
