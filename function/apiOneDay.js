93e971089c8a0ca1640eb898c054ad389936637e
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-09-12 23:19:19 +0200

    design 7 days finished

--- function/apiOneDay.js
+++ function/apiOneDay.js
@@ -14,22 +14,18 @@ const apiOneDay = {
         const weather = {
             lon: weatherResponse.coord.lon,
             lat: weatherResponse.coord.lat,
-
             name: weatherResponse.name,
             icon: icon.getIconOneDay(weatherResponse.weather[0].icon),
             main: weatherResponse.main.humidity,
             description:  weatherResponse.weather[0].description,
-
             temp: Math.round((weatherResponse.main.temp * 100) / 100),
             temp_max: Math.round(weatherResponse.main.temp_max),
             temp_min: Math.round(weatherResponse.main.temp_min),
             pressure: weatherResponse.main.pressure,
             humidity: weatherResponse.main.humidity,
-   
             winddeg: weatherResponse.wind.deg,
             clouds: weatherResponse.clouds.all,
             speed: Math.round(weatherResponse.wind.speed * 3.6),
-
             sunset: callfunction.sunsetAndSunrise(weatherResponse.sys.sunset),
             sunrise: callfunction.sunsetAndSunrise(weatherResponse.sys.sunrise),
         }
